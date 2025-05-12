409e6a49822faa429f4cf1d97921bc2166e064aa
mudiyanmas <nithinluro16@gmail.com>
2025-05-12 16:02:32 +0530

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
-const { malvin } = require("../malvin");
+const { liza } = require("../liza");
 
-malvin({
+liza({
   pattern: "tiktok",
   alias: ["ttdl", "tiktokdl","tt"],
   react: '📥',
