e188ab1581b9d6a0619cf3ecb112c236c7d955a2
mudiyanmas <nithinluro16@gmail.com>
2025-05-12 13:21:34 +0530

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
-const { malvin } = require("../malvin");
+const { liza } = require("../liza");
 
-malvin({
+liza({
   pattern: "igimagedl",
   alias: ["instagramimages", "igimages","igimage"],
   react: '📥',
