756a375a1abf703d3112c8135977ce4b8549f4fc
LIL CRONE <tcronebtech2015@gmail.com>
2025-07-05 03:18:48 +0200

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -7,9 +7,9 @@ const {runtime} = require('../lib/functions')
 const axios = require('axios')
 
 cmd({
-    pattern: "menu2",
-    alias: ["allmenu","fullmenu"],
-    use: '.menu2',
+    pattern: "menu",
+    alias: ["menu","fullmenu"],
+    use: '.menu',
     desc: "Show all bot commands",
     category: "menu",
     react: "📜",
